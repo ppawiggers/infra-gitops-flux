05784c56ac06c55cead1893152c97591ce7c23a5
Peter Wiggers <peter@bitlayer.nl>
2025-04-18 16:19:05 +0200

    fix

--- scripts/k3d-init.sh
+++ scripts/k3d-init.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 CLUSTER="$1"
 
-kubectl apply -f "clusters/$CLUSTER/flux-system"
+kubectl apply --server-side -f "clusters/$CLUSTER/flux-system/gotk-components.yaml"
+flux check
+kubectl apply --server-side -f "clusters/$CLUSTER/flux-system/gotk-sync.yaml"
